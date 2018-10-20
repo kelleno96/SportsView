2bb3d135c68e2939dd3d6dc8e746f8a2fdf65b39
Andrew Hancock <AndrewHancock0@Gmail.com>
2018-10-20 05:01:51 -0500

    orientation

--- webcam_initial_test.py
+++ webcam_initial_test.py
@@ -1,9 +1,9 @@
 import socket
 import cv2
 from math import *
-
 import numpy as np
 from OrangeBoxGetter import GetOrangeBoxCenter
+import matplotlib.pyplot as plt
 
 camera_matrix = np.array([[852.8381859353582, 0.0, 642.3604852739107],
                           [0.0, 849.8663434244934, 342.35385770005394],
@@ -17,19 +17,25 @@ zed_matrix = np.array([[699.555, 0, 658.919],
 
 dist_coeff_zed = np.array([-.16912, .021884, 0, 0])
 
-cap = cv2.VideoCapture(0)
-cap.set(cv2.CAP_PROP_FRAME_WIDTH, 1280)
-cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 720)
+cap = cv2.VideoCapture(1)
+cap.set(3, 1280)
+cap.set(4, 720)
 
-zed = cv2.VideoCapture(1)
+zed = cv2.VideoCapture(2)
 zed.set(3, 2560)
 zed.set(4, 720)
 
 # camera locations
 c1_loc = (0, 0)
-c2_loc = (73.5 * 25.4, 0)
+c2_loc = (90.5 * 25.4, 0)
+c1_ori = -45.0
+c2_ori = 0.0
+
+fig = plt.figure()
+fig.add_subplot(111);
 
 while 1:
+
     # get two images
     ret, frame = cap.read()
     frame = cv2.undistort(frame, camera_matrix, dist_coeff)
@@ -40,11 +46,6 @@ while 1:
     xzed = GetOrangeBoxCenter(zedFrame, "Zed")
 
     # put boxes around and get coordinates
-
-    # x_loc_l = 803
-    # y_loc_l = 53
-    # x_loc_r = 624
-    # y_loc_r = 23
     x_loc_l = x
     x_loc_r = xzed
     Cx = 642
@@ -52,20 +53,39 @@ while 1:
     CxZed = 658.919
     FxZed = 699.555
 
+
+
     # figure out location
+    beta = 0
+    alpha = 0
+    theta_u_right = degrees(atan(abs(x_loc_r - CxZed) / FxZed))
+    if x_loc_r - CxZed < 0:
+    	beta = 90 - theta_u_right
+    else:
+    	beta = 90+ theta_u_right
 
-    beta = 90 - degrees(atan(abs(x_loc_r - CxZed) / FxZed))
-    alpha = 90 - degrees(atan(abs(x_loc_l - CxZed) / FxZed))
+    theta_u_left = degrees(atan(abs(x_loc_l - Cx) / Fx))
+    if x_loc_l - Cx > 0:
+    	alpha = 90 - theta_u_left
+    else:
+    	alpha = 90 + theta_u_left
 
-    # print(beta)
-    # print(alpha)
-    # print(abs(c1_loc[0] - c2_loc[0]))
-    # print(degrees(sin(radians(beta))))
+    alpha += c1_ori
+    beta += c2_ori
+
     B = (abs(c1_loc[0] - c2_loc[0])) * sin(radians(beta)) / sin(radians(180 - alpha - beta))
     A = (abs(c1_loc[0] - c2_loc[0])) * sin(radians(alpha)) / sin(radians(180 - alpha - beta))
 
-    y_loc_obj = B * sin(radians(alpha))
-    x_loc_obj = B * cos(radians(alpha))
+    y_loc_obj = B * sin(radians(alpha))/1000
+    x_loc_obj = B * cos(radians(alpha))/1000
 
-    print("x, y: " + str(x_loc_obj / 1000) + ", " + str(y_loc_obj / 1000))
+    print("x, y: " + str(x_loc_obj) + ", " + str(y_loc_obj))
+
+    x = [c1_loc[0], c2_loc[0], (x_loc_obj)]
+    y = [c1_loc[1], c2_loc[1], (y_loc_obj)]
+
+    plt.cla()
+    plt.scatter(x, y)
+    #plt.pause(0.05)
+    
 
